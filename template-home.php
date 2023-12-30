8d664e8c886b673af93272b367a3f3a35787f407
ViniciusOLucio <138497678+ViniciusOLucio@users.noreply.github.com>
2023-12-30 01:13:45 -0300

    js

--- template-home.php
+++ template-home.php
@@ -5,17 +5,31 @@
 <section class="hero sessao container">
 
   <div class="hero-txt">
-    <div class="hero-txt-h1">
-      <h1>
-        Vinicius um
-        <span>
-          web designer
-        </span> e
-        <span>
-          front-end developer
-        </span>
-      </h1>
-    </div>
+  <div class="hero-txt-h1">
+  <h1 id="textoH1">
+    Vinicius um
+    <span id="web-designer">
+      web designer
+    </span> e
+    <span id="front-end-developer">
+      front-end developer
+    </span>
+  </h1>
+</div>
+
+<script>
+  function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+      setTimeout(() => elemento.innerHTML += letra, 75 * i);
+    });
+  }
+
+  const titulo = document.querySelector(' h1');
+  typeWriter(titulo);
+</script>
+
     <div class="hero-txt-p">
       <p>
         Bem-vindo ao meu espaço digital! Sou Vinícius, um desenvolvedor front-end apaixonado por transformar ideias em experiências memoráveis.
@@ -134,6 +148,56 @@
     </div>
   </div>
 </section>
+<!-- fim sobre -->
 
+
+<section id="projetos" class="sessao">
+
+<div class="introducao_conteudo container">
+    <div class="introducao_conteudo-txt">
+      <span>
+        #
+      </span>
+      <h2>
+        projetos
+      </h2>
+    </div>
+    <div class="introducao_conteudo_wrapper"></div>
+
+  </div>
+  <!-- fim introducao -->
+    <div class="projetos container">
+      <ul class="lista-projetos">
+        <li class="card-projeto">
+          <div class="card-projeto-img">
+            <a href="https://harturobessa.com.br" target="_blank">
+            <img src="<?php echo get_template_directory_uri() ?>/assets/img/ReadIcon.svg" alt="">
+            </a>
+          </div>
+          <div class="card-projeto-txt">
+            <p>
+              <span>
+
+              </span>
+            </p>
+            <h2>
+
+            </h2>
+            <h3>
+
+            </h3>
+
+            <div class="btn">
+            <a href="" class="btn-secundario">
+              <img src="<?php echo get_template_directory_uri() ?>/assets/img/ArrowIcon.svg" alt="">
+              Ver mais
+            </a>
+          </div>
+          </div>
+        </li>
+
+      </ul>
+    </div>
+</section>
 
 <?php get_footer(); ?>
