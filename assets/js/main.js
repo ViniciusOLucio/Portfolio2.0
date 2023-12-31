220daf0d6e32192c871e773c6bfadf4a024a81b9
ViniciusOLucio <138497678+ViniciusOLucio@users.noreply.github.com>
2023-12-31 13:54:09 -0300

    aleteracoes

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+window.addEventListener('load',function(){
+  function typeWriter(elementos) {
+    let ultimoIndex = 0;
+
+    elementos.forEach(function (elemento){
+      const textoArray = elemento.innerText.split("");
+      elemento.innerText = "";
+      textoArray.forEach(function(letra)  {
+      setTimeout(function() {
+        elemento.innerText = elemento.innerText + letra;
+      }, 75 * ultimoIndex );
+      ultimoIndex = ultimoIndex + 1;
+    });
+  });
+   }
+   const titulo = document.querySelector("#textoH1");
+   titulo.classList.remove("hidden");
+  const titulos = document.querySelectorAll("#textoH1 > * ");
+
+  typeWriter(titulos);
+
+  typeWriter(document.querySelectorAll("#trabalho-atual > a "))
+
+  //
+});
+
+
